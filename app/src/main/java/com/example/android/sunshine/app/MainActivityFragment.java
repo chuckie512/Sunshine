fface0ed6b21f86cc59c4f3a08c62c09c9a0b427
Charles Smith <chuckie512@gmail.com>
2015-06-03 23:46:58 -0400

    added test data

--- app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -18,6 +23,26 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] weatherData = {
+                                "Today - Sunny - 88/74",
+                                "Tomorrow - Partly Cloudy - 80/70",
+                                "Weds - cloudy - 75/68",
+                                "Thurs - rainy - 60/52",
+                                "Fri - Flaming - 700/451",
+                                "Sat - Death - 0/0",
+                                "sun - Partly Cloudy - 78/69",
+        };
+
+        ArrayAdapter forecastAdapter = new ArrayAdapter(getActivity(),R.layout.list_item_forcast, R.id.list_item_forcast_textview, weatherData);
+
+        ListView weatherListView = (ListView) rootView.findViewById(R.id.listViewForcast);
+
+        weatherListView.setAdapter(forecastAdapter);
+
+
+        return rootView;
     }
 }
